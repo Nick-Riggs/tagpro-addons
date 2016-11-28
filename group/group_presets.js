5c8956af3d523405af10d970f11ad6b48e047a25
Ruud Verbeek <ruud@arfie.nl>
2016-11-28 19:57:51 +0100

    auto-detect selected preset on page load

--- group/group_presets.js
+++ group/group_presets.js
@@ -21,6 +21,25 @@
         set: function(key, value) {
             tagpro.group.socket.emit('setting', {name: key, value: value});
         },
+        get: function(key) {
+            switch(key) {
+                case 'buffDelay':
+                    return $('input[name=buffDelay]').prop('checked');
+
+                case 'redTeamName':
+                case 'blueTeamName':
+                    return $('input[name=' + key + ']').val();
+
+                case 'redTeamScore':
+                    return $('#red-team .team-score-value').text();
+
+                case 'blueTeamScore':
+                    return $('#blue-team .team-score-value').text();
+
+                default:
+                    return $('select[name=' + key + ']').val();
+            }
+        },
         apply: function(preset) {
             console.log(preset);
             for(let k in tagpro.group.settings.defaults)
@@ -60,6 +79,7 @@
         ]
     };
     
+    let presetSelected = false;
     
     // DOM manipulation, you'll probably want to do this in a more convenient way if you implement this :)
     let presetSelector = $('<select class="form-control">');
@@ -67,7 +87,24 @@
     for(let i=0; i<tagpro.group.settings.presets.length; ++i) {
         let preset = tagpro.group.settings.presets[i];
         presetSelector.append($('<option>').prop('value', i).text('Preset: ' + preset.name));
+        
+        let selected = true;
+        for(let k in tagpro.group.settings.defaults) {
+            if(tagpro.group.settings.get(k) != (preset.hasOwnProperty(k) ? preset[k] : tagpro.group.settings.defaults[k])) {
+                selected = false;
+                break;
+            }
+        }
+        if(selected) {
+            presetSelected = true;
+            presetSelector.val(i);
+        }
     }
+    if(!presetSelected) {
+        let option = $('<option>').prop('value', -1).text('Preset: None');
+        presetSelector.prepend(option).val(-1);
+    }
+    
     presetSelector.change(function() {
         tagpro.group.settings.apply(tagpro.group.settings.presets[presetSelector.val()]);
     });
