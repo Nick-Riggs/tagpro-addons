9d032ac732298f514efbfdf484806988ebf3e14c
Ron Marks <romarks@ucsc.edu>
2016-11-28 16:07:07 -0800

    add a lot of tips

--- global/TipOnLoadingPage.js
+++ global/TipOnLoadingPage.js
@@ -16,7 +16,7 @@ var tips = [
 	"Try to not grab the flag when the opponents are coming in for a capture. If you die there will be one less person to try and stop them.",
 	"Consider waiting if there are not 8 players in the game yet to make things fair.",
 	"Powerups respawn one minute after they are last picked up. If you pay attention to the game clock while taking one, you will be able to be there when it respawns and take it again.",
-	"Instead of grabbing the flag when you have a tagpro powerup, try to pop the defenders to give another attacker an easy grab.",
+	"If you have a tagpro on offense, then focus on clearing out the enemy defenders and let a teammate pick up the flag.",
 	"A rolling bomb powerup works as a one-time shield when you are carrying the flag. If someone on your team has the powerup, let them take the flag.",
 	"Rolling on tiles of your color make you move a lot faster. If you are chasing down an attacker, try to take a route along these team tiles to catch up.",
 	"Boosts respawn 10 seconds after they are used. Bombs respawn after 30 seconds and powerups after 1 minute.",
@@ -29,8 +29,18 @@ var tips = [
 	"If you like the game check out the /r/TagPro subreddit to keep up with the latest news!",
 	"TagPro has a few servers on the voice chat server Mumble, you should get on there if you want to play some serious games and meet fellow players (microphone not needed).",
 	"If you see someone break a rule you can report them by clicking on their ball, their name in chat or the report button on the scoreboard.",
-	"If someone gives you a false report you can go to IRC #TPmods on freenode to have moderators remove the report for you.",
-	"Don't get mad at your team for doing things wrong! Instead, give them tips on how to improve on playing the game so they can get better."
+	"Don't get mad at your team for doing things wrong! Instead, give them tips on how to improve on playing the game so they can get better.",
+	"Team tiles don't make you go faster if you're holding the flag.",
+	"If you're trapped in your end zone in a neutral flag game, pop yourself to reset the flag.",
+	"Be mindful of when your powerup is going to run out. Powerups last for 20 seconds.",
+	"Leave the rolling bomb powerup for your flag carrier if they are nearby.",
+	"When you are trying to grab, use boosts, bombs, and powerups to disrupt the enemy defense in order to get to the flag. The more chaos you can cause, the harder it is to defend the flag.",
+	"You have 0.25 seconds of immunity after grabbing the flag. Use this to your advantage.",
+	"You don’t always need to go full speed when you have the flag. If you move slowly, you can change directions faster and have a better chance of juking enemy chasers.",
+	"When you are trying to snipe a flag carrier, don’t aim directly for them. You need to predict where they are going to be and aim for that area instead.",
+	"Do not guard an empty base unless you are 100% sure that your flag is going to get returned or if you can’t stop the enemy from capping.",
+	"Walls can be used to quickly change direction when you are trying to juke other players.",
+	"Have fun. If you are having a losing streak or you are getting too frustrated then take a break or turn your stats off for a while. Typically, you’ll play a lot worse when you’re frustrated."
 ];
 
 // Find spot in DOM to place tip
