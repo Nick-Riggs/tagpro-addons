7909ce03d87cd2a03f19a4462b6e5e925c1d516a
Pranav Mathur <pranavmathur99@gmail.com>
2016-12-27 10:42:03 -0800

    Fix grammar in tips script (#11)
    Added Oxford comma in line #32 to normalize style with line #38.

--- findGame/TipOnLoadingPage.js
+++ findGame/TipOnLoadingPage.js
@@ -7,17 +7,17 @@
 $(document).ready(function() {
     var num = 0;
     var tips = [
-        "When chasing the opponent's flag carrier always stay between them and the flag tile.",
+        "When chasing the opponent's flag carrier, always stay between them and the flag tile.",
         "Nobody on regrab? Get on regrab. Somebody on regrab? Chase the player holding your flag.",
         "Pressing your keys harder doesn't make you go faster. Don't break your keyboard!",
         "When you have the flag, it's not always best to cap quickly. Be patient and wait for your team to put in blocks.",
         "Always have two people stay in base to prevent the other team from taking your flag.",
         "Stay near the opponent's base so you can regrab the flag if your teammate pops.",
         "Watch your speed when chasing. Move too fast and you might overcommit. Too slow and they'll go right past you!",
-        "Try to not grab the flag when the opponents are coming in for a capture. If you die there will be one less person to try and stop them.",
+        "Try to not grab the flag when the opponents are coming in for a capture. If you die, there will be one less person to try and stop them.",
         "Consider waiting if there aren't 8 players in the game to make things fair.",
         "Powerups respawn one minute after they are last picked up.",
-        "When you have a tagpro on offense let a teammate pick up the flag and focus on clearing a path.",
+        "When you have a tagpro on offense, let a teammate pick up the flag and focus on clearing a path.",
         "A rolling bomb powerup works as a one-time shield when you're carrying the flag.",
         "Rolling on tiles of your color make you move a lot faster.",
         "Boosts respawn 10 seconds after they are used. Bombs respawn after 30 seconds. Powerups after 1 minute.",
@@ -29,7 +29,7 @@ $(document).ready(function() {
         "Earn degrees&deg; by winning games and unlock cool flairs.",
         "Check out the <a href='https://reddit.com/r/tagpro'>/r/TagPro subreddit</a> to read the latest news!",
         "Meet TagPro players on our <a href='https://reddit.com/r/tagpro/wiki/mumble'>voice-chat mumble servers</a>. Microphone not required.",
-        "If you see someone break a rule you can report them by clicking on their ball, their name in chat or the report button on the scoreboard.",
+        "If you see someone break a rule you can report them by clicking on their ball, their name in chat, or the report button on the scoreboard.",
         "Don't get mad at your team! Instead, give them tips on how to improve.",
         "Team tiles don't make you go faster if you're holding the flag.",
         "When you're trapped in your end zone in a neutral flag game, pop yourself to reset the flag.",
@@ -40,7 +40,7 @@ $(document).ready(function() {
         "If you move slowly, you can change directions faster and have a better chance of juking enemies.",
         "Don't guard an empty base! Return the flag.",
         "Walls can be used to quickly change direction when you are trying to juke other players.",
-        "Have fun! If you're on a losing streak or getting frustrated then take a break."
+        "Have fun! If you're on a losing streak or getting frustrated, take a break."
     ];
 
     // Places random tip in proper spot in DOM
